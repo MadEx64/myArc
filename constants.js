9aec6e20396cf9ddd609bb1e432c53a883035d29
Mickael Beites <mrouceirobeites@gmail.com>
2025-06-03 22:50:10 +0200

    refactor: migrate constants from JavaScript to TypeScript with updated header size

--- constants.js
+++
@@ -1,3 +0,0 @@
-export const ARCHIVE_MAGIC = "MYARC";
-export const ARCHIVE_VERSION = 1;
-export const ARCHIVE_HEADER_SIZE = 13;
---
+++ constants.ts
@@ -0,0 +1,3 @@
+export const ARCHIVE_MAGIC = "MYARC"; // 5 bytes
+export const ARCHIVE_VERSION = 1;
+export const ARCHIVE_HEADER_SIZE = 10; // 5 (magic) + 1 (version) + 4 (fileCount) 
